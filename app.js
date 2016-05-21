11d64d9ba0f06a730f59b16e237ab176f830c743
Alexander Thohoh <jerk401@gmail.com>
2016-05-21 15:24:42 +0300

    .

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');
+var Message = require('./models/message-model');
 
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/scripts", express.static(__dirname + "/node_modules/"));
 
 var routes = require('./routes/routes.js')(app);
 
 io.on('connection', function (socket) {
     socket.on('chat_message', function (msg) {
-        io.emit('chat_message');
+        var newMessage = new Message(msg);
+        newMessage.save(function (err) {
+            if (err) { return console.error(err.message); }
+            io.emit("chat_message", msg);
+        });
     });
+});
+
+mongoose.connect(config.MONGO_URI + config.MONGO_COLLECTION);
+mongoose.connection.on('error', function(err) {
+    console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
 });
 
 server.listen(3000, function () {
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,16 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Message = require('../models/message-model');
+
 var appRouter = function (app) {
     app.get('/fetch', function (req, res) {
-        res.send({message: 'woot'});
+        Message.find( {}, function(error, messages) {
+            if(error) {
+                return res.status(400).send(error);
+            }
+            return res.send(messages);
+        });
     })
 };
 
